7ed76dc0de2593ed05b1101416c5d531af0fc92d
LujustiaN <luan08100@gmail.com>
2022-04-17 21:42:09 +0700

    auto format

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,101 +1,109 @@
+var productArray = [{
+        productId: 10001,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-1.jpg',
+        name: 'Đồng hồ dán tường mica',
+        price: 765000,
+        pricedel: 100000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+    },
+    {
+        productId: 10002,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-2.jpg',
+        name: 'Đồng hồ đầu hưu hoa tài lộc',
+        price: 545000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
 
-var productArray = [
-	{
-		productId:10001,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-1.jpg', 
-		name:'Đồng hồ dán tường mica', 
-		price:765000,
-		pricedel: 100000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	},
-	{
-		productId:10002,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-2.jpg', 
-		name:'Đồng hồ đầu hưu hoa tài lộc', 
-		price:545000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
 
-	
-	},
-	{
-		productId:10003,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-3.jpg', 
-		name:'Chậu cây xanh mini', 
-		price:595000,
-		pricedel: 900000,
-		description: `<b>description sẽ hiện cạnh hình ảnh chi tiết sản phẩm.</b> <br> 
+    },
+    {
+        productId: 10003,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-3.jpg',
+        name: 'Chậu cây xanh mini',
+        price: 595000,
+        pricedel: 900000,
+        description: `<b>description sẽ hiện cạnh hình ảnh chi tiết sản phẩm.</b> <br> 
 		Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...`,
-		detailDescription: `
+        detailDescription: `
 			Tên sản phẩm: chậu cây xanh decor vintage. <br>
 			Chất liệu: lá xanh nhựa+ chậu đất nung vẽ giả vintage.  <br>
 			Kích thước: nhiều size. chậu cây cao 10cm, 13cm, 26cm và cao 32cm.  <br>
 			Tham khảo thêm các mẫu hoa giả đẹp khác tại: <a href="https://greenholo.htcnw.kienlab.com/products.html">đây</a>`
-	},
-	// feedmore-slick
-	{
-		productId:10004,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-4.jpg', 
-		name:'Đồng hồ Vintage 58cm 03', 
-		price:595000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm.Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	
-	},
-	{
-		productId:10005,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-5.jpg', 
-		name:'Đồng hồ Vintage 58cm 03', 
-		price:595000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	
-	},
-	{
-		productId:10006,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-6.jpg', 
-		name:'Đồng hồ Vintage 58cm 03', 
-		price:595000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	
-	},
-	{
-		productId:10007,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-7.jpg', 
-		name:'Đồng hồ Vintage 58cm 03', 
-		price:595000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	
-	},
-	{
-		productId:10008,
-		brand:'OEM',
-		img:'assets/images/selling-products/pic-8.jpg', 
-		name:'Đồng hồ Vintage 58cm 03', 
-		price:595000,
-		pricedel: 900000,
-		description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
-		detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
-	
-	}
+    },
+    // feedmore-slick
+    {
+        productId: 10004,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-4.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm.Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    },
+    {
+        productId: 10005,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-5.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    },
+    {
+        productId: 10006,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-6.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    },
+    {
+        productId: 10007,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-7.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    },
+    {
+        productId: 10008,
+        brand: 'OEM',
+        img: 'assets/images/selling-products/pic-8.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    },
+    {
+        productId: 10009,
+        brand: 'OEM',
+        img: 'assets/images/clock/pic-9.jpg',
+        name: 'Đồng hồ Vintage 58cm 03',
+        price: 595000,
+        pricedel: 900000,
+        description: 'description sẽ hiện cạnh hình ảnh chi tiết sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...',
+        detailDescription: ' Detail Description sẽ hiện dưới Thông tin sản phẩm. Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những ...'
+
+    }
 ];
 
-function getProductSlice(start, end) 
-{
-	return productArray.slice(start, end);
+function getProductSlice(start, end) {
+    return productArray.slice(start, end);
 }
